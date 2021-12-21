7fcbc3aa1f33f11175c06aada2abb1d9ee94a9f3
naveen <naveenneevan73@gmail.com>
2021-12-21 14:31:02 +0530

    register page updated

--- app/src/main/java/com/example/pddetectv1/register.java
+++ app/src/main/java/com/example/pddetectv1/register.java
@@ -22,30 +22,20 @@ public class register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        setContentView(R.layout.register);
+        setContentView(R.layout.activity_register);
         submit = findViewById(R.id.submit);
         name = findViewById(R.id.name);
         age = findViewById(R.id.age);
         gender = findViewById(R.id.spinnertext);
         height = findViewById(R.id.height);
         weight = findViewById(R.id.weight);
-        registerhome=findViewById(R.id.homeregister);
-        //Home hide & disable
-        registerhome.setEnabled(false);
-        registerhome.setVisibility(View.INVISIBLE);
+
         final AutoCompleteTextView genderAuto = findViewById(R.id.spinnertext);
 
         ArrayList<String> genderList = getgender();
         ArrayAdapter<String> adapter = new ArrayAdapter<>(register.this, android.R.layout.simple_spinner_item, genderList);
         fillDetails();
         genderAuto.setAdapter(adapter);
-        registerhome.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent homeregisterintent=new Intent(register.this, homepage.class);
-                startActivity(homeregisterintent);
-            }
-        });
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -58,7 +48,7 @@ public class register extends AppCompatActivity {
                     profileEdit.putFloat("height",Float.parseFloat(height.getText().toString()));
                     profileEdit.putFloat("weight",Float.parseFloat(weight.getText().toString()));
                     profileEdit.commit();
-                    Intent homeIntent = new Intent(register.this, homepage.class);
+                    Intent homeIntent = new Intent(register.this, MainActivity.class);
                     startActivity(homeIntent);
                 } catch (Exception e) {
                     Toast.makeText(getApplicationContext(), "Fill all the details!", Toast.LENGTH_LONG).show();
@@ -80,8 +70,6 @@ public class register extends AppCompatActivity {
         String nameValue = profileCheck.getString("name","");
         if(!nameValue.equals("")){
             // Home button enable & visible
-            registerhome.setEnabled(true);
-            registerhome.setVisibility(View.VISIBLE);
             submit.setText("UPDATE PROFILE");
 
             Integer ageValue = profileCheck.getInt("age", 0);
